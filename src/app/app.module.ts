85b795e3f4328e8ff4706fcb0bb96399afa3fb42
Tanya Samal <solesytto@gmail.com>
2022-03-22 16:14:29 +0300

    feat: generate all the necessary components using ng-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { FilterComponent } from './filter/filter.component';
+import { SearchCardComponent } from './search/search-card/search-card.component';
+import { CardStatisticComponent } from './search/search-card/card-statistic/card-statistic.component';
+import { SearchFieldComponent } from './header/search-field/search-field.component';
+import { UserInfoComponent } from './header/user-info/user-info.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, SearchComponent, FilterComponent, SearchCardComponent, CardStatisticComponent, SearchFieldComponent, UserInfoComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/search/search-card/card-statistic/card-statistic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-statistic',
+  templateUrl: './card-statistic.component.html',
+  styleUrls: ['./card-statistic.component.css']
+})
+export class CardStatisticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
