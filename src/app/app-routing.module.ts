2e1a015e9c2d66ec5ce02349a5e7f38df875b252
Tanya Samal <solesytto@gmail.com>
2022-03-27 14:22:09 +0300

    feat: add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/pages/not-found/not-found.component';
 import { SearchPageComponent } from './youtube/pages/search-page/search-page.component';
-// import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: SearchPageComponent },
-  // { path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,16 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchFieldComponent } from './components/header/search-field/search-field.component';
 import { UserInfoComponent } from './components/header/user-info/user-info.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, SearchFieldComponent, UserInfoComponent],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    SearchFieldComponent,
+    UserInfoComponent,
+    NotFoundComponent,
+  ],
   exports: [HeaderComponent],
   imports: [CommonModule, SharedModule],
 })
