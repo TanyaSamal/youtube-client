3b6240b4c96a5c962ca1e709fcbbac623cecf487
Tanya Samal <solesytto@gmail.com>
2022-03-29 21:43:10 +0300

    feat: show filter button only on serch page

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { StateService } from '../../services/state.service';
 
 @Component({
@@ -6,13 +8,27 @@ import { StateService } from '../../services/state.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   isFilterShown = false;
+  isSearchPage = false;
+  sub: Subscription = new Subscription();
 
-  constructor(private stateService: StateService) {}
+  constructor(private stateService: StateService, private router: Router) {}
+
+  ngOnInit() {
+    this.sub = this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.isSearchPage = val.url === '/search';
+      }
+    });
+  }
 
   showFilter() {
     this.isFilterShown = !this.isFilterShown;
     this.stateService.updatedDataSelection(this.isFilterShown);
+  }
+
+  ngOnDestroy() {
+    if (this.sub) this.sub.unsubscribe();
   }
 }
