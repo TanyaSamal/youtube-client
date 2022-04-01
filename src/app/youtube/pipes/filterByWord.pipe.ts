1f4ab2b9310db274c6ae484103c6ddb263d4a90b
Tanya Samal <solesytto@gmail.com>
2022-04-01 20:33:10 +0300

    fix: delete unnesesary files and folders

--- src/app/youtube/pipes/filterByWord.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { ISearchCard } from '../models/search-card.model';
-
-@Pipe({ name: 'filterByWord' })
-export class FilterByWordPipe implements PipeTransform {
-  public transform(items: ISearchCard[], word: string): any {
-    return items.filter((a) => a.snippet.title.toLowerCase().indexOf(word.toLowerCase()) !== -1);
-  }
-}
